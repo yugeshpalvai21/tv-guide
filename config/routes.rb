1c45ca0e10e2ccd96b6206442e2b42a0b5a10040
Yugesh Palvai <pyreddy517@hotmail.com>
2020-02-22 01:38:58 +0530

    Implements Functionality of Add Favourite Show To User

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/search', to: 'pages#search'
-  resources :shows, only: [:index, :show]
+  resources :shows, only: [:index, :show] do
+    member do
+      post 'add_favourite'
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_21_194320) do
+ActiveRecord::Schema.define(version: 2020_02_21_194759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2020_02_21_194320) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "favourites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "show_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["show_id"], name: "index_favourites_on_show_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
   end
 
   create_table "shows", force: :cascade do |t|
@@ -42,5 +51,7 @@ ActiveRecord::Schema.define(version: 2020_02_21_194320) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favourites", "shows"
+  add_foreign_key "favourites", "users"
   add_foreign_key "shows", "channels"
 end
