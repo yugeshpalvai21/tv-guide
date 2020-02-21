29eb155b0fc3601f25b88a003af885a835849e20
Yugesh Palvai <pyreddy517@hotmail.com>
2020-02-22 00:30:58 +0530

    Implements Basic Search Functionality for Channel or Show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def search
+    @channels = Channel.where('LOWER(name) LIKE ?', "%#{params[:search_term].to_s.downcase}%")
+    @shows = Show.where('LOWER(name) LIKE ?', "%#{params[:search_term].to_s.downcase}%")
+  end
 end
